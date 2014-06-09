98f4a271ddcc31e0f0da3b3960ca9c71937b367d
hadley <h.wickham@gmail.com>
2014-06-09 16:10:21 -0500

    Actually save file

--- data-raw/storms.R
+++ data-raw/storms.R
@@ -5,7 +5,7 @@ storms <- read.csv("data-raw/stormtracks.csv", stringsAsFactors = FALSE)
 names(storms) <- tolower(names(storms))
 
 storms <- storms %>%
-  select(storms, name:hour, lat = latitude, long = longitude, pressure:seasday) %>%
+  select(name:hour, lat = latitude, long = longitude, pressure:seasday) %>%
   tbl_df()
 
 save(storms, file = "data/storms.rdata")
